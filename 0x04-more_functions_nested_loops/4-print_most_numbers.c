cb035ef9de164b4610e2c20d95c4b921abc2ab80
Moyosore Omoniyi <moyosoreo@hardcorebiometric.com>
2022-03-17 23:39:24 +0100

    Added file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers from 0 to 9, without printing 2 and 4.
+ * print_most_numbers - prints numbers between 0 to 9
  * Return: void.
  */
 void print_most_numbers(void)
