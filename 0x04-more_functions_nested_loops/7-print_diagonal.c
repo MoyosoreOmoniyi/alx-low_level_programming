c9827de5855febc9fbb915c8e6f43717ff10d190
Moyosore Omoniyi <moyosoreo@hardcorebiometric.com>
2022-03-19 03:18:35 +0100

    Modified file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,16 @@ void print_diagonal(int n)
 	int j;
 	if (n <= 0)
 		_putchar('\n');
-
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; j++)
+	else {
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
-	}
 	_putchar('\n');
+	}
 }
