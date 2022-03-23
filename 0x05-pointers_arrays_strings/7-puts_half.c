bf9449505c8d83a9c444114bde7447a3e098cb44
Moyosore Omoniyi <moyosoreo@hardcorebiometric.com>
2022-03-23 05:00:13 +0100

    Added file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int length = 0, i;
+
+	while (length >= 0)
+	{
+		if (str[length] != '\0')
+		{
+			length++;
+		} else
+		{
+			break;
+		}
+	}
+
+	if (length % 2 == 1)
+		n = length / 2;
+	else
+		n = (length - 1) / 2;
+
+	for (i = 0; i < n; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
