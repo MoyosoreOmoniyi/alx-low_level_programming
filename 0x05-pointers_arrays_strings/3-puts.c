4fa465a0b6e802aa710ceb155a70c01ffe6fa3a0
Moyosore Omoniyi <moyosoreo@hardcorebiometric.com>
2022-03-23 04:08:08 +0100

    Added file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line to stdout.
+ * @str: input string.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int count = 1;
+
+	while (count > 1)
+	{
+		if (str[count] != '\0')
+		{
+			_putchar(str[count - 1]);
+			count++;
+		}
+		count == 0;
+	}
+}
