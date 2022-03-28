1fed5f82932a6260cb0fd17ff7664ae03084d5f9
Moyosore Omoniyi <moyosoreo@hardcorebiometric.com>
2022-03-28 16:10:19 +0100

    Added file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings,
+ * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
+ * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int size1 = 0;
-	int size2 = 0;
+	int size1 = 0, size2 = 0;
 
 	while (*(dest + size1) != '\0')
 	{
 		size1++;
 	}
 
-	while (size2 >= 0)
+	while (size2 < n)
 	{
 		*(dest + size1) = *(src + size2);
 		if (*(src + size2) == '\0')
